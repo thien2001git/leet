f05fbf80d4d673f355fc61fe43ff57ebb2f23144
thien2001git <hoangthien66771508@gmail.com>
2026-03-07 15:38:45 +0700

    🎯 [Feature] isPalindrome

--- src/main/java/palindromenumber/Solution.java
+++ src/main/java/palindromenumber/Solution.java
@@ -2,6 +2,42 @@ package palindromenumber;
 
 public class Solution {
     public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+        final String s = String.valueOf(x);
+
+        int toR = 0;
+        int toL = s.length() - 1;
+        while (toR <= toL) {
+            if (s.charAt(toR) != s.charAt(toL)) {
+                return false;
+            }
+            ++toR;
+            --toL;
+        }
+
         return true;
+    }
+
+    public boolean isPalindrome2(int x) {
+        final int factor = 10;
+        if (x < 0 || (x % factor == 0 && x != 0)) {
+            return false;
+        }
+
+        if (x < factor) {
+            return true;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * factor + x % factor;
+            x /= factor;
+        }
+        return x == revertedNumber || x == revertedNumber / factor;
     }
 }
--- src/test/java/palindromenumber/SolutionTest.java
+++ src/test/java/palindromenumber/SolutionTest.java
@@ -12,12 +12,16 @@ class SolutionTest {
         // Define test cases: {Input String, NumRows, Expected Output}
         final Object[][] cases = {
                 {121, true},
+                {-121, false},
+                {1, true},
+                {10, false},
         };
 
         for (final Object[] test : cases) {
             final int input = (int) test[0];
             final boolean expected = (boolean) test[1];
             Assertions.assertEquals(expected, solution.isPalindrome(input));
+            Assertions.assertEquals(expected, solution.isPalindrome2(input));
         }
     }
 }
